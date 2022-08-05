1d27536734101a224ce623400fe99a593dbdcef9
LautaroNicolas <tato.nicolas@hotmail.com>
2022-08-05 15:00:19 -0300

    homework2-completed

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -182,10 +182,10 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-  if(valor==true){
+  if(valor===true){
     return ('Soy verdadero')
   }
-  if(valor==false){
+  if(valor===false){
     return 'Soy falso'
   }
 
